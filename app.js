27c69c811c165efb34ed6c621b057e688f035342
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2021-12-16 19:34:49 +0200

    server update

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //importing modules
-const express= require('express');
-const path= require('path');
-const bodyparser= require('body-parser');
-const cors= require('cors');
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const cors = require("cors");
 //--------------------------------------------------------------------------
 
 //importing router files
-const user_router= require('./routes/user_router');
-const item_router= require('./routes/item_router');
-const order_router= require('./routes/order_router');
+const user_router = require("./routes/user_router");
+const item_router = require("./routes/item_router");
+const order_router = require("./routes/order_router");
 //--------------------------------------------------------------------------------
 
 //initialise express
-const app= express();
+const app = express();
 //--------------------------------------------------------------------------------
 
 //adding middlewares
@@ -22,13 +22,19 @@ app.use(bodyparser.json());
 
 //initialising routes
 app.use("/user", user_router);
-app.use("/item",item_router);
-app.use("/order",order_router);
+app.use("/item", item_router);
+app.use("/order", order_router);
 //------------------------------------------------------------------------------------
 
 //set static folders
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads',express.static(path.join(__dirname,'uploads')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 //------------------------------------------------------------------------------------------
 
-module.exports= app;
+//catch all get requests that dont match any route in the server
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+//------------------------------------------------------------------------------------------
+
+module.exports = app;
