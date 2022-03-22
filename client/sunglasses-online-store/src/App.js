a54842de2bcce7dd2f26e1a5661068106864d098
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-03-22 19:10:40 +0200

    VerifyEmailForTestingOnly component completed

--- client/sunglasses-online-store/src/App.js
+++ client/sunglasses-online-store/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import EditEmail from "./components/edit-email/EditEmail";
+import VerifyEmailForTestingOnly from "./components/verify-email/VerifyEmailForTestingOnly";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="app" data-testid="App-div">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<EditEmail />} />
+          <Route path="/" element={<VerifyEmailForTestingOnly />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/sunglasses-online-store/src/components/verify-email/VerifyEmailForTestingOnly.js
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import axios from "axios";
+const VerifyEmailForTestingOnly = () => {
+  const error = useRef(null);
+  const success = useRef(null);
+  //form submit handler
+  const verifyEmail = async (e) => {
+    try {
+      await axios.post(
+        process.env.REACT_APP_BASE_URL + "user/verify_email_for_testing_only"
+      );
+      success.current.textContent = "Email successfully verified";
+      success.current.classList.remove("d-none");
+    } catch (err) {
+      error.current.childNodes[0].textContent = "Error: " + err.response.data;
+      error.current.classList.remove("d-none");
+    }
+  };
+  //handle error alert close button click
+  const handleErrorAlertCloseClick = () => {
+    error.current.classList.add("d-none");
+  };
+  //The component
+  return (
+    <div className="container-fluid text-center">
+      <h3 className="my-5">Verify your email</h3>
+      <button className="btn btn-primary mb-3" onClick={verifyEmail}>
+        Verify Email
+      </button>
+      <div
+        className="alert alert-danger alert-dismissible text-start d-none"
+        ref={error}
+      >
+        <div>error</div>
+        <button
+          className="btn-close"
+          onClick={handleErrorAlertCloseClick}
+        ></button>
+      </div>
+      <div className="alert alert-success text-start d-none" ref={success}>
+        success
+      </div>
+    </div>
+  );
+};
+export default VerifyEmailForTestingOnly;
