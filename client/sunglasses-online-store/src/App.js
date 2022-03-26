720e7fc03d50a0679dfe24a5369bc8e7470851e5
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-03-26 10:36:46 +0200

    CustomerOrder component completed

--- client/sunglasses-online-store/src/App.js
+++ client/sunglasses-online-store/src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
-import AccountSettings from "./components/account-settings/AccountSettings";
+import CustomerOrder from "./components/customer-order/CustomerOrder";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  let order = {
+    item_name: "Ray-Ban Rounded",
+    item_size: "50 150-50",
+    item_color: "Gold metal, black glasses",
+    item_price: 230,
+    quantity: 2,
+    date_time: new Date(Date.now()),
+    item_brand: "Ray-Ban",
+    status: "Delivered",
+    item_id: "1234",
+    item_info: {
+      images: ["blabla.jpg"],
+    },
+  };
   return (
     <div className="app" data-testid="App-div">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AccountSettings />} />
+          <Route path="/" element={<CustomerOrder order={order} />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/sunglasses-online-store/src/components/customer-order/CustomerOrder.js
@@ -0,0 +1,34 @@
+import React from "react";
+const CustomerOrder = (props) => {
+  let statusBootstrapColor = "text-info";
+  if (props.order.status === "Delivered") statusBootstrapColor = "text-success";
+  return (
+    <div className="bg-light border rounded m-4 d-flex flex-nowrap">
+      <img
+        className="me-2 rounded align-self-center"
+        src={props.order.item_info.images[0]}
+        alt={props.order.item_name}
+        width="100"
+        height="100"
+      ></img>
+      <div className="d-flex flex-wrap flex-grow-1 align-content-center">
+        <span className="me-2">{props.order.item_name}</span>
+        <span className="me-2">{props.order.item_color}</span>
+        <span className="me-2">{props.order.item_size}</span>
+        <span className="me-2">{props.order.item_price}$</span>
+        <span className="me-2">Quantity: {props.order.quantity}</span>
+        <span className="me-2">
+          Total Price: {props.order.item_price * props.order.quantity}$
+        </span>
+        <span className="me-2">
+          Ordered in: {props.order.date_time.toLocaleDateString()}{" "}
+          {props.order.date_time.toLocaleTimeString()}
+        </span>
+      </div>
+      <span className={"me-2 align-self-center " + statusBootstrapColor}>
+        {props.order.status}
+      </span>
+    </div>
+  );
+};
+export default CustomerOrder;
