835021aa1d56eb3ad3109d75a85684dc02b47790
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-04-09 18:49:10 +0200

    Item component completed

--- client/sunglasses-online-store/src/App.js
+++ client/sunglasses-online-store/src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
-import UserItems from "./components/user-items/UserItems";
+import Item from "./components/item/Item";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const item = {
+    name: "Ray-Ban Rounded",
+    brand: "Ray-Ban",
+    size: "50 150-50",
+    color: "gold",
+    quantity: 5,
+    times_ordered: 100,
+    price: 250,
+    _id: "1234",
+    images: [
+      "ray-ban7.jpg",
+      "ray-ban7.jpg",
+      "ray-ban7.jpg",
+      "ray-ban7.jpg",
+      "ray-ban7.jpg",
+    ],
+  };
   return (
     <div className="app" data-testid="App-div">
       <BrowserRouter>
         <Routes>
           <Route
             path="/"
-            element={<UserItems requestQuery={{}} loggedIn={true} />}
+            element={
+              <Item item={item} viewImage={() => console.log("hello")} />
+            }
           />
         </Routes>
       </BrowserRouter>
---
+++ client/sunglasses-online-store/src/components/item/Item.js
@@ -0,0 +1,37 @@
+import React from "react";
+const Item = (props) => {
+  return (
+    <div className="m-4 d-inline-flex">
+      <div style={{ maxWidth: "200px" }} className="me-2">
+        <img
+          src={props.item.images[0]}
+          alt={props.item.name}
+          className="rounded border mb-2"
+          style={{ maxWidth: "200px" }}
+        />
+        {props.item.images.map((image, index) => (
+          <button className="btn me-2 mb-2 p-0 border" key={index}>
+            <img
+              src={image}
+              alt={index}
+              className="rounded"
+              style={{ maxWidth: "40px" }}
+              onClick={props.viewImage}
+            />
+          </button>
+        ))}
+      </div>
+      <div style={{ maxWidth: "200px" }}>
+        <div>ID: {props.item._id}</div>
+        <div>Brand: {props.item.brand}</div>
+        <div>Name: {props.item.name}</div>
+        <div>Color: {props.item.color}</div>
+        <div>Size: {props.item.size}</div>
+        <div>Price: {props.item.price}$</div>
+        <div>Quantity: {props.item.quantity}</div>
+        <div>Times Ordered: {props.item.times_ordered}</div>
+      </div>
+    </div>
+  );
+};
+export default Item;
